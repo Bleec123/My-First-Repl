b00fd87f92ccefa7dc079706ee8bee5dc5511497
Bleec <107743786+Bleec123@users.noreply.github.com>
2022-06-20 19:30:56 +0000

    print each letter in name

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+
+
 print("Hello World!")
+
 name = input("Hi, what's your name?")
 
-print("Hello {}" .format(FIRST_NAME))
+print("Hello {}".format(name))
+
+for letter in name:
+  print(letter)
+
+print("Welcome to my first REPL")
